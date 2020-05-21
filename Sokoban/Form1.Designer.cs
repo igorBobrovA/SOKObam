94b58b5615855b057b152bc5b2a3870e9946cfc6
igorBobrovA <57355113+igorBobrovA@users.noreply.github.com>
2020-05-21 22:10:49 +0700

    123

---
+++ Sokoban/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Sokoban
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.этоМенюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.темаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.уровниToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lvl1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lvl2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lvl3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lvl4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lvl5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.этоМенюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(586, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // этоМенюToolStripMenuItem
+            // 
+            this.этоМенюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.играToolStripMenuItem,
+            this.темаToolStripMenuItem,
+            this.уровниToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.этоМенюToolStripMenuItem.Name = "этоМенюToolStripMenuItem";
+            this.этоМенюToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.этоМенюToolStripMenuItem.Text = "Это меню";
+            this.этоМенюToolStripMenuItem.Click += new System.EventHandler(this.этоМенюToolStripMenuItem_Click);
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.играToolStripMenuItem.Text = "Новая игра";
+            this.играToolStripMenuItem.Click += new System.EventHandler(this.играToolStripMenuItem_Click);
+            // 
+            // темаToolStripMenuItem
+            // 
+            this.темаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.темаToolStripMenuItem.Name = "темаToolStripMenuItem";
+            this.темаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.темаToolStripMenuItem.Text = "Темы";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem2.Text = "1";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem3.Text = "2";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(80, 22);
+            this.toolStripMenuItem4.Text = "3";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // уровниToolStripMenuItem
+            // 
+            this.уровниToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lvl1,
+            this.lvl2,
+            this.lvl3,
+            this.lvl4,
+            this.lvl5});
+            this.уровниToolStripMenuItem.Name = "уровниToolStripMenuItem";
+            this.уровниToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.уровниToolStripMenuItem.Text = "Уровни";
+            // 
+            // lvl1
+            // 
+            this.lvl1.Name = "lvl1";
+            this.lvl1.Size = new System.Drawing.Size(180, 22);
+            this.lvl1.Text = "1";
+            this.lvl1.Click += new System.EventHandler(this.lvl1_Click);
+            // 
+            // lvl2
+            // 
+            this.lvl2.Name = "lvl2";
+            this.lvl2.Size = new System.Drawing.Size(180, 22);
+            this.lvl2.Text = "2";
+            this.lvl2.Click += new System.EventHandler(this.lvl1_Click);
+            // 
+            // lvl3
+            // 
+            this.lvl3.Name = "lvl3";
+            this.lvl3.Size = new System.Drawing.Size(180, 22);
+            this.lvl3.Text = "3";
+            this.lvl3.Click += new System.EventHandler(this.lvl1_Click);
+            // 
+            // lvl4
+            // 
+            this.lvl4.Name = "lvl4";
+            this.lvl4.Size = new System.Drawing.Size(180, 22);
+            this.lvl4.Text = "4";
+            this.lvl4.Click += new System.EventHandler(this.lvl1_Click);
+            // 
+            // lvl5
+            // 
+            this.lvl5.Name = "lvl5";
+            this.lvl5.Size = new System.Drawing.Size(180, 22);
+            this.lvl5.Text = "5";
+            this.lvl5.Click += new System.EventHandler(this.lvl1_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(586, 592);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(586, 612);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Sokoban";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem этоМенюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem темаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem уровниToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lvl1;
+        private System.Windows.Forms.ToolStripMenuItem lvl2;
+        private System.Windows.Forms.ToolStripMenuItem lvl3;
+        private System.Windows.Forms.ToolStripMenuItem lvl4;
+        private System.Windows.Forms.ToolStripMenuItem lvl5;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Sokoban/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Sokoban
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        PictureBox box, box2, Place, Walls, player;
+        int playerX, playerY, WIN = 0, theme = 1, lvl = 1, WallCount = 1, BoxCount = 1;
+        bool HODI;
+        string BOXimg,PLACEimg,inPLACEimg,PLAYERimg;
+
+        private void этоМенюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvl1_Click(object sender, EventArgs e)
+        {
+            var Whichlvl = sender as ToolStripMenuItem;
+            lvl = Convert.ToInt32(Whichlvl.Text);
+            startGame();
+            WIN = 0;
+            //lvl = Convert.ToInt32(Whichlvl.Name.Substring(2, 1));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            timer1.Enabled = false;
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e){theme = 3;Visial(); }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e){theme = 2;Visial(); }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e){theme = 1;Visial();}
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            HODI = true;
+             playerX = player.Location.X;
+             playerY = player.Location.Y;
+            switch (e.KeyCode)
+            {
+                case Keys.S: findBox(0, 50);break;
+                case Keys.W: findBox(0, -50); break;
+                case Keys.D: findBox(50, 0); break;
+                case Keys.A: findBox(-50, 0); break;
+            }
+        }
+
+        public void findBox(int ChangeX, int ChangeY)
+        {
+            for (int i = 1; i < BoxCount; i++)
+            {
+                box = Controls["Box" + i] as PictureBox;
+                if (box.Location.X == playerX + ChangeX && box.Location.Y == playerY + ChangeY)
+                {
+                    for (int j = 1; j < BoxCount; j++)
+                    {
+                        box2 = Controls["Box" + j] as PictureBox;
+                        if (box2.Location.X == playerX + ChangeX * 2 && box2.Location.Y == playerY + ChangeY * 2)
+                        {
+                            HODI = false; break;
+                        }
+                    }
+                        for (int a = 1; a < WallCount; a++)
+                        {
+                            Walls = Controls["wallBox" + a] as PictureBox;
+                        if (Walls.Location.X == playerX + ChangeX * 2 && Walls.Location.Y == playerY + ChangeY * 2)
+                        {
+                            HODI = false; break;
+                        }
+                        }
+                        if (HODI)
+                        {
+                            Color1(PLACEimg, -1);
+                            box.Location = new Point(playerX + ChangeX * 2, playerY + ChangeY * 2);
+                            Color1(inPLACEimg, 1);
+                        }
+                break;
+                }
+            }
+            for (int a = 1; a < WallCount; a++)
+            {
+                Walls = Controls["wallBox" + a] as PictureBox;
+                if (Walls.Location.X == playerX + ChangeX && Walls.Location.Y == playerY + ChangeY)
+                {
+                    HODI = false; break;
+                }
+            }
+            if (HODI) player.Location = new Point(playerX + ChangeX, playerY + ChangeY);
+            if (WIN ==  BoxCount-1)
+            {
+                if (lvl == 5)//
+                {
+                    DialogResult res = MessageBox.Show("Вы прошли игру\nХотите начать новую игру", "Поздравляю", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (res == DialogResult.Yes)
+                    {
+                        lvl = 1;
+                    }
+                    else if (res == DialogResult.No) startGame();
+                }
+                else
+                {
+                    DialogResult res = MessageBox.Show("Вы прошли уровень\nхотите начать новый уровень???\nЧтобы пройти на " + (lvl+1) + " нажмите\n 'ДА'\nЧтобы перезапустить " + lvl + " уровень\n 'НЕТ'", "Congretilations", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (res == DialogResult.Yes)
+                    {
+                        lvl++;
+                    }
+                    else if (res == DialogResult.No) startGame();
+                }
+                WIN = 0;
+                startGame();
+            }
+        }
+        public void Color1(string color ,int ChangeWin)
+        {
+            for (int j = 1; j < BoxCount; j++)
+            {
+                Place = Controls["PLACEbox" + j] as PictureBox;
+                if (box.Location == Place.Location)
+                {
+                    Place.Image = Image.FromFile(color);
+                    WIN += ChangeWin;
+                    break;
+                }
+            }
+        }
+        public void startGame()
+        {
+            
+            for (int i = 1; i < WallCount; i++)
+            {
+                Controls.Remove(Controls["wallBox" + i]);
+            }
+                Controls.Remove(Controls["playerBox1"]);
+            for (int i = 1; i < BoxCount; i++)
+            {
+                Controls.Remove(Controls["Box" + i]);
+                Controls.Remove(Controls["PLACEbox" + i]);
+            }
+            Level(lvl);
+            Visial();
+            label1.Size = new Size(this.Width, this.Height);
+            label1.Visible = true;
+            label1.Text = "Уровень" + lvl;
+            timer1.Enabled = true;
+            label1.BringToFront();
+        }
+        public void Level(int lvl)
+        {
+            using(StreamReader sr = new StreamReader("levels/level" + lvl + "Walls.txt"))
+            {
+                WallCount = 1;
+
+                while (!sr.EndOfStream)
+                {
+                    string[] Cord = sr.ReadLine().Split(',');
+                    Walls = new PictureBox()
+                    {
+                        Name = "wallBox" + WallCount,
+                        Size = new Size(50, 50),
+                        Location = new Point(Convert.ToInt32(Cord[0]), Convert.ToInt32(Cord[1])),
+                        // Image = Image.FromFile(),
+                        BackColor = Color.White,
+                    };
+                    Controls.Add(Walls);
+                    WallCount++;
+                }
+            }
+            using(StreamReader sr = new StreamReader("levels/level" + lvl + "Box.txt"))
+            {
+                BoxCount = 1;
+                while (!sr.EndOfStream)
+                {
+                    string[] BoxOrPlace = sr.ReadLine().Split(';');
+                    string[] BoxCord = BoxOrPlace[0].Split(',');
+                    box = new PictureBox()
+                    {
+                        Name = "Box" + BoxCount,
+                        Size = new Size(50, 50),
+                        Location = new Point(Convert.ToInt32(BoxCord[0]), Convert.ToInt32(BoxCord[1])),
+                        SizeMode = PictureBoxSizeMode.StretchImage
+                    };
+                    Controls.Add(box);
+                    box.SendToBack();
+                    string[] PlaceCord = BoxOrPlace[1].Split(',');
+                    Place = new PictureBox()
+                    {
+                        Name = "PLACEbox" + BoxCount,
+                        Size = new Size(50, 50),
+                        Location = new Point(Convert.ToInt32(PlaceCord[0]), Convert.ToInt32(PlaceCord[1])),
+                        SizeMode = PictureBoxSizeMode.StretchImage
+                    };
+                    Controls.Add(Place);
+                    Place.BringToFront();
+                    BoxCount++;
+                }
+            }
+            using(StreamReader sr = new StreamReader("levels/level" + lvl + "Player.txt"))
+            {
+                string[] cord1 = sr.ReadLine().Split(',');
+                player = new PictureBox()
+                {
+                    Name = "playerBox1",
+                    Size = new Size(50, 50),
+                    Location = new Point(Convert.ToInt32(cord1[0]), Convert.ToInt32(cord1[1])),
+                    SizeMode = PictureBoxSizeMode.StretchImage
+                };
+                Controls.Add(player);
+                player.BringToFront();
+                cord1 = sr.ReadLine().Split(',');
+                this.Size = new Size(Convert.ToInt32(cord1[0]), Convert.ToInt32(cord1[1]));
+            }
+        }
+        public void Visial()
+        {
+            player = Controls["playerBox1"] as PictureBox;
+            BOXimg = "Theme/Theme" + theme + "/Box.jpg";
+            PLACEimg = "Theme/Theme" + theme + "/gem.jpg";
+            inPLACEimg = "Theme/Theme" + theme + "/BoxOk.jpg";
+            PLAYERimg = "Theme/Theme" + theme + "/player.jpg";
+            for (int i = 1; i < BoxCount; i++)
+            {
+                Place = Controls["PLACEbox" + i] as PictureBox;
+                for (int j = 1; j < BoxCount; j++)
+                {
+                    box = Controls["Box" + j] as PictureBox;
+                    if (Place.Location == box.Location)//
+                    {
+                        Place.Image = Image.FromFile(inPLACEimg);
+                        break;
+                    }
+                    else Place.Image = Image.FromFile(PLACEimg);
+                }
+                box = Controls["Box" + i] as PictureBox;
+                box.Image = Image.FromFile(BOXimg);
+            }
+            player.Image = Image.FromFile(PLAYERimg);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            startGame();
+            Visial();
+        }
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void играToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            startGame();
+        }
+    }
+}
